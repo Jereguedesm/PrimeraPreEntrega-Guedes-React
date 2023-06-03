e4921f23ab70a6925d4dd981821459fa0aa41a7c
Jeremias <jeremiasguedes04@gmail.com>
2023-06-03 16:34:17 -0300

    V-0.2

---
+++ CursoReact/src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import "./NavBar.css"
+import "./header.css"
+const NavBar = () => {
+    <div className="Header">
+            <h1>TiendaOnline</h1>
+            <div>
+            <ul className="UL">
+                <li className="LI">Inicio</li>
+                <li className="LI">Productos</li>
+                <li className="LI">Carrito</li>
+                <li className="LI"><img src="../assets/svgCarrito.svg" alt="" /></li>
+            </ul>
+            <img src="../assets/svgCarrito.svg" alt="" />
+            </div>
+        </div>
+}
+
+export default NavBar
